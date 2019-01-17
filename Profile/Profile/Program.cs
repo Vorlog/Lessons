bfb19625f9d0dd38555fcdb0949107931d07ed21
Anton <vorlog@yandex.ru>
2019-01-18 00:09:28 +0300

    Create first task

--- Profile/Profile/Program.cs
+++ Profile/Profile/Program.cs
@@ -10,6 +10,24 @@ namespace Profile
     {
         static void Main(string[] args)
         {
+            // Задание 1. Анкета
+            Console.WriteLine("Введите ваше имя");
+            String first_name = Console.ReadLine();
+            Console.WriteLine("Введите вашу фамилию");
+            String second_name = Console.ReadLine();
+            Console.WriteLine("Введите ваш возраст");
+            String input = Console.ReadLine();
+            int age = Int32.Parse(input);
+
+            // Вывод данных а (конкатенация)
+            Console.WriteLine("Имя: "+first_name+" "+second_name+". "+"Возраст: "+age );
+            // Вывод данных б (форматирование)
+            Console.WriteLine("Имя: {0} {1}. Возраст: {2}", first_name,second_name,age);
+            // Вывод данных в* (интерполяция)
+            Console.WriteLine($"Имя: {first_name} {second_name}. Возраст: {age}");
+
+            Console.ReadKey();
+
         }
     }
 }
